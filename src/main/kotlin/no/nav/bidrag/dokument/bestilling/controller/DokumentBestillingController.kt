a7277e634340cc1f1a6a851e692687540308464a
Ugur Alpay Cenar <cenar93@gmail.com>
2022-11-10 17:50:44 +0100

    Refactor and add better swagger support (#2)

--- src/main/kotlin/no/nav/bidrag/dokument/bestilling/controller/DokumentBestillingController.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/bestilling/controller/DokumentBestillingController.kt
@@ -1,22 +1,17 @@
 package no.nav.bidrag.dokument.bestilling.controller
 
-import com.ibm.mq.constants.CMQC
-import com.ibm.msg.client.jms.JmsConstants
+import io.micrometer.core.annotation.Timed
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import no.nav.bidrag.dokument.bestilling.SECURE_LOGGER
 import no.nav.bidrag.dokument.bestilling.model.BrevKode
 import no.nav.bidrag.dokument.bestilling.model.DokumentBestillingRequest
 import no.nav.bidrag.dokument.bestilling.model.DokumentBestillingResponse
 import no.nav.bidrag.dokument.bestilling.service.DokumentBestillingService
 import no.nav.security.token.support.core.api.Protected
-import no.nav.security.token.support.core.api.Unprotected
-import no.nav.security.token.support.spring.SpringTokenValidationContextHolder
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.ResponseEntity
-import org.springframework.jms.core.JmsTemplate
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -26,11 +21,8 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @Protected
-class DokumentBestillingController(
-    var dokumentBestillingService: DokumentBestillingService,
-    var onlinebrevTemplate: JmsTemplate,
-    @Value("\${BREVSERVER_KVITTERING_QUEUE}") var replyQueueName: String
-    ) {
+@Timed
+class DokumentBestillingController(var dokumentBestillingService: DokumentBestillingService) {
 
     companion object {
         private val LOGGER = LoggerFactory.getLogger(DokumentBestillingController::class.java)
@@ -42,8 +34,12 @@ class DokumentBestillingController(
         security = [SecurityRequirement(name = "bearer-key")],
     )
     @ApiResponses(value = [ApiResponse(responseCode = "400", description = "Dokument ble bestilt med ugyldig data")])
-    fun bestillBrev(@RequestBody request: DokumentBestillingRequest, @PathVariable brevKode: BrevKode): DokumentBestillingResponse {
-        return dokumentBestillingService.bestill(request, brevKode)
+    fun bestillBrev(@RequestBody bestillingRequest: DokumentBestillingRequest, @PathVariable brevKode: BrevKode): DokumentBestillingResponse {
+        LOGGER.info("Bestiller dokument for brevkode $brevKode og enhet ${bestillingRequest.enhet}")
+        SECURE_LOGGER.info("Bestiller dokument for brevkode $brevKode med data $bestillingRequest og enhet ${bestillingRequest.enhet}")
+        val result = dokumentBestillingService.bestill(bestillingRequest, brevKode)
+        LOGGER.info("Bestilt dokument for brevkode $brevKode og enhet ${bestillingRequest.enhet} med respons $result")
+        return result
     }
 
     @RequestMapping("/brevkoder", method = [RequestMethod.OPTIONS])
@@ -56,22 +52,6 @@ class DokumentBestillingController(
             LOGGER.info("Hentet støttede brevkoder $it")
             it
         }
-    }
-
-    @PostMapping("/bestill/raw")
-    fun bestillRaw(@RequestBody request: String): ResponseEntity<Void> {
-        onlinebrevTemplate.send {
-            val message = it.createTextMessage()
-            val rq = com.ibm.mq.jms.MQQueue(replyQueueName)
-            message.jmsReplyTo = rq
-            message.setIntProperty(JmsConstants.JMS_IBM_ENCODING, CMQC.MQENC_S390)
-            message.setIntProperty(JmsConstants.JMS_IBM_CHARACTER_SET, 277)
-            message.setIntProperty(JmsConstants.JMS_IBM_MSGTYPE, CMQC.MQMT_DATAGRAM)
-            message.setIntProperty(JmsConstants.JMS_IBM_PUTAPPLTYPE, CMQC.MQAT_CICS)
-            message.text = request
-            message
-        }
-        return ResponseEntity.ok().build()
     }
 
 }
