4e45e990d5d3630b54c1c5d2bda7b1c0a486efa4
Ugur Cenar <cenar93@gmail.com>
2023-05-02 17:20:07 +0200

    Refactor and dependencies upgrade

--- src/main/kotlin/no/nav/bidrag/dokument/bestilling/konfigurasjon/SaksbehandlerInfoManager.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/bestilling/konfigurasjon/SaksbehandlerInfoManager.kt
@@ -1,8 +1,6 @@
 package no.nav.bidrag.dokument.bestilling.konfigurasjon
 
-import no.nav.bidrag.commons.security.service.OidcTokenManager
 import no.nav.bidrag.commons.security.utils.TokenUtils
-import no.nav.bidrag.commons.security.utils.TokenUtils.fetchSubject
 import no.nav.bidrag.dokument.bestilling.konsumer.BidragOrganisasjonConsumer
 import no.nav.bidrag.dokument.bestilling.model.Saksbehandler
 import org.springframework.stereotype.Service
@@ -10,11 +8,10 @@ import org.springframework.stereotype.Service
 @Service
 class SaksbehandlerInfoManager(
     private val bidragOrganisasjonConsumer: BidragOrganisasjonConsumer,
-    private val oidcTokenManager: OidcTokenManager
 ) {
     fun hentSaksbehandlerBrukerId(): String? {
         return try {
-            fetchSubject(oidcTokenManager.fetchTokenAsString())
+            TokenUtils.hentSaksbehandlerIdent()
         } catch (e: Exception) {
             null
         }
@@ -32,7 +29,7 @@ class SaksbehandlerInfoManager(
 
     fun erSystembruker(): Boolean {
         return try {
-            TokenUtils.isSystemUser(oidcTokenManager.fetchTokenAsString())
+            TokenUtils.erApplikasjonsbruker()
         } catch (e: Exception) {
             false
         }
